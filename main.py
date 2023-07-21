ba2b0edffe726762d7e8efd33a0281dcdab0b0f9
Urban-Orange <125282075+Urban-Orange@users.noreply.github.com>
2023-07-21 15:52:03 +0100

    Update main.py
    Edited so if a language is needed it will only search for those repos using that language by conducting a check.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ else:
 
 load_dotenv()  # Load environment variables from .env file
 
-def fetch_github_repositories(search_term, language=None):
+def fetch_github_repositories(search_term, language=None, max_results=100):
     access_token = os.getenv("GITHUB_ACCESS_TOKEN")
 
     if not access_token:
@@ -19,16 +19,37 @@ def fetch_github_repositories(search_term, language=None):
 
     base_url = "https://api.github.com/search/repositories"
     headers = {"Authorization": f"Bearer {access_token}"}
-    params = {"q": search_term, "language": language} if language else {"q": search_term}
+    
+    if language:
+        language = language.lower()
+    
+    params = {"q": search_term, "language": language, "per_page": 100} if language else {"q": search_term, "per_page": 100}
 
-    response = requests.get(base_url, params=params, headers=headers)
-    response_json = response.json()
+    repositories = []
+    page = 1
 
-    if response.status_code == 200:
-        return response_json["items"]
-    else:
-        print(f"Failed to fetch repositories. Status code: {response.status_code}")
-        return []
+    while True:
+        params["page"] = page
+        response = requests.get(base_url, params=params, headers=headers)
+        response_json = response.json()
+
+        if response.status_code == 200:
+            current_repositories = response_json["items"]
+            if not current_repositories:
+                break
+            repositories.extend(current_repositories)
+            page += 1
+            if len(repositories) >= max_results:
+                break
+        else:
+            print(f"Failed to fetch repositories. Status code: {response.status_code}")
+            break
+
+    # Filter repositories to match the specified language
+    if language:
+        repositories = [repo for repo in repositories if repo['language'] and repo['language'].lower() == language]
+
+    return repositories[:max_results]
 
 def display_repositories(repositories):
     for repo in repositories:
